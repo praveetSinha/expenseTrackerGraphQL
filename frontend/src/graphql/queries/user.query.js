4e7e7bbe1a6c100946a85625a6f79da965b71961
praveetSinha <praveettis@gmail.com>
2025-06-16 23:33:27 +0530

    cron.js added

--- frontend/src/graphql/queries/user.query.js
+++ frontend/src/graphql/queries/user.query.js
@@ -18,7 +18,7 @@ export const GET_USER_AND_TRANSACTIONS = gql`
 			name
 			username
 			profilePicture
-			# relationships
+			# possible with the help of relationships
 			transactions {
 				_id
 				description
